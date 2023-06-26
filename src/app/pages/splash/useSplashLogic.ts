49a7af6a6e505e33c6d90d06ba9942de2eb8f79d
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2023-06-26 03:04:50 +0100

    update useSplashLogic

--- src/app/pages/splash/useSplashLogic.ts
+++ src/app/pages/splash/useSplashLogic.ts
@@ -1,12 +1,42 @@
 import { useEffect } from "react";
 
+import useLoadWeatherList from '../../hooks/useLoadWeatherList';
+import useFetchWeather from '../../hooks/useFetchWeather';
+import useGeolocation from '../../hooks/useGeolocation';
+import { TypeGeolocation } from "../../types";
 
-const useSplashLogic = (f7route: any) => {
+
+const useSplashLogic = (f7router: any) => {
+
+    const { currentWeather } = useLoadWeatherList();
+    const { loading, error, fetchWeather } = useFetchWeather();
+    const { currentLocation } = useGeolocation();
+
 
     useEffect(() => {
-        setTimeout(() => {
-            f7route.navigate('/weather/');
-        }, 3000);
+
+        const navigateOptions = {
+            reloadCurrent: true
+        };
+
+        if (!currentWeather) {
+            if (currentLocation) {
+                fetchWeather((location as unknown) as TypeGeolocation);
+            } else {
+                setTimeout(() => {
+                    f7router.navigate('/search/', navigateOptions);
+                }, 2000);
+            }
+        } else {
+            fetchWeather(currentWeather.getName());
+        }
+
+        if (loading === 'succeeded') {
+            setTimeout(() => {
+                f7router.navigate('/weather/', navigateOptions);
+            }, 2000);
+        }
+
     }, []);
 };
 
